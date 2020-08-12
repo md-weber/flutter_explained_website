5782365ea344281b2bcba4c8fabc220e65df91b0
myracledesign <weber.max.1988@live.de>
2020-08-12 11:50:26 +0200

    add an lottie file to the project

--- lib/ui/screens/home_screen/hero_image.dart
+++ lib/ui/screens/home_screen/hero_image.dart
@@ -1,8 +1,16 @@
 import "package:flutter/material.dart";
+import 'package:lottie/lottie.dart';
 
 class HeroImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.asset("assets/images/code_thinking.png");
+    // TODO: Change Sizedbox depending on screensize
+    return SizedBox(
+      child: LottieBuilder.asset(
+        "assets/lottie/mobile_homescreen.json",
+        width: 500,
+        height: 500,
+      ),
+    );
   }
 }
