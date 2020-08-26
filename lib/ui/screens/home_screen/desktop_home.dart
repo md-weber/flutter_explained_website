edaa160565259972a99395e05e7e942bba7c3091
myracledesign <weber.max.1988@live.de>
2020-08-26 10:36:39 +0200

    made the first page scrollable

--- lib/ui/screens/home_screen/desktop_home.dart
+++ lib/ui/screens/home_screen/desktop_home.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_explained_website/ui/screens/home_screen/hero_image.dart';
 import 'package:flutter_explained_website/ui/screens/home_screen/jumbotron.dart';
+import 'package:flutter_explained_website/utils/size_config.dart';
 
 const kBackgroundDecoration = BoxDecoration(
   color: Color(0x00FFFFFF),
@@ -11,11 +12,20 @@ class DesktopHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: kBackgroundDecoration,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: ListView(
+        shrinkWrap: true,
         children: [
-          Expanded(flex: 6, child: Jumbotron()),
-          Expanded(flex: 8, child: HeroImage()),
+          SizedBox(
+            height: 80 * SizeConfig.heightMultiplier,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(flex: 6, child: Jumbotron()),
+                Expanded(flex: 8, child: HeroImage()),
+              ],
+            ),
+          ),
+          const Text("Hello World")
         ],
       ),
     );
